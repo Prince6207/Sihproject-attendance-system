edca756b98895c3e0da19692f0a5c7bcb22285ff
Harshkapoor975 <harshkapoor972005@gmail.com>
2025-09-13 18:20:25 +0530

    trying connention frontend to backend on localhost stage 1

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,20 @@
 import express from "express";
-import mongoose from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 import qrRoutes from "./routes/qr.routes.js";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/attendance", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log(" MongoDB connected"))
-  .catch(err => console.error(err));
+// Connect DB
+connectDB();
 
+// Routes
 app.use("/api/qr", qrRoutes);
 
-app.listen(5000, () => console.log(" Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
