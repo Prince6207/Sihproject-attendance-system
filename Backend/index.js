697486121226b018dc42951644b23c7b33ca1f42
Harshkapoor975 <harshkapoor972005@gmail.com>
2025-09-17 21:48:48 +0530

    deploy

--- Backend/index.js
+++ Backend/index.js
@@ -48,5 +48,5 @@ console.log("qr creation error4") ;
 
 // const PORT = process.env.PORT || 5000;
 app.listen(() => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port`);
 });
