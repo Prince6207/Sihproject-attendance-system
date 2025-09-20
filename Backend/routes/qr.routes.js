fafaf5b10dadee5b1335ccec6851462747151178
Harshkapoor975 <harshkapoor972005@gmail.com>
2025-09-20 11:48:58 +0530

    sih started

--- Backend/routes/qr.routes.js
+++ Backend/routes/qr.routes.js
@@ -2,9 +2,9 @@ import express from "express";
 import { generateQR, verifyQR } from "../controllers/qrcode.controller.js";
 
 const router = express.Router();
-console.log("qr creation error2") ;
+// console.log("qr creation error2") ;
 router.get("/generate", generateQR);   // GET /api/qr/generate?sessionId=123
-console.log("qr creation error3") ;
+// console.log("qr creation error3") ;
 router.post("/verify", verifyQR);      // POST /api/qr/verify
 
 export default router;
